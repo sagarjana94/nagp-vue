7c29bbfcf9a67e3d338b6d233a941ba0201afb1d
Sagar Jana <sagar.jana@nagarro.com>
2019-10-21 04:02:31 +0530

    NAGP Vue assignment: UI fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="home-page">
-    <div class="banner">
-      <div class="container">
-        <h1 class="logo-font">NAGP Blogger</h1>
-        <p>A place to share your knowledge.</p>
-      </div>
-    </div>
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
